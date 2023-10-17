71817379fefb29faff8de541d084998a02f5583c
Muneeb-Usman <muneebusman54@gmail.com>
2023-10-17 10:28:55 +0500

    Implement basic filtering for Name and Age

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const sampleData = [
     { id: 1, name: 'John Doe', age: 25, city: 'New York', occupation: 'Engineer' },
@@ -15,9 +15,54 @@ const sampleData = [
 ];
 
 function App() {
+    const [data, setData] = useState(sampleData);
+    const [filters, setFilters] = useState({
+        name: '',
+        age: '',
+    });
+
+    const handleFilterChange = (e) => {
+        const { name, value } = e.target;
+        setFilters({ ...filters, [name]: value });
+    };
+
+    const handleResetFilters = () => {
+        setFilters({
+            name: '',
+            age: '',
+        });
+        setData(sampleData);
+    }
+
+    // Filter the data based on user input
+    const filteredData = data.filter((item) => {
+        return (
+            item.name.toLowerCase().includes(filters.name.toLowerCase()) &&
+            (filters.age === '' || item.age.toString() === filters.age)
+        );
+    });
+
     return (
         <div className="App">
-            <h1>Static Data Table</h1>
+            <h1>Filterable Table</h1>
+
+            {/* Filter Inputs */}
+            <div className="filters">
+                <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    value={filters.name}
+                    onChange={handleFilterChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Age"
+                    name="age"
+                    value={filters.age}
+                    onChange={handleFilterChange}
+                />
+            </div>
 
             {/* Display the Table */}
             <table>
@@ -31,7 +76,7 @@ function App() {
                 </tr>
                 </thead>
                 <tbody>
-                {sampleData.map((item) => (
+                {filteredData.map((item) => (
                     <tr key={item.id}>
                         <td>{item.id}</td>
                         <td>{item.name}</td>
